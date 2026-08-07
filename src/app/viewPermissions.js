9f6cbb119487bcc2e23e152f21089c3769abb343
wangmiao033 <161289342+wangmiao033@users.noreply.github.com>
2026-08-07 17:19:01 +0800

    Map app views to RBAC permissions

---
+++ src/app/viewPermissions.js
@@ -0,0 +1,36 @@
+import { VIEWS } from '@/app/routes.js'
+
+export const VIEW_PERMISSIONS = Object.freeze({
+  [VIEWS.BUSINESS_DASHBOARD]: 'analytics.view',
+  [VIEWS.PROFIT_ANALYSIS]: 'analytics.view',
+  [VIEWS.ANOMALIES]: 'anomalies.view',
+  [VIEWS.BANK_RECONCILIATION]: 'funds.view',
+  [VIEWS.BANK_TRANSACTIONS_LEDGER]: 'funds.view',
+  [VIEWS.BANK_STATEMENT_IMPORT]: 'funds.manage',
+  [VIEWS.RECON_RD]: 'reconciliation.view',
+  [VIEWS.RECON_PROGRESS]: 'reconciliation.view',
+  [VIEWS.RECON_CREATE]: 'reconciliation.manage',
+  [VIEWS.RECON_EDIT]: 'reconciliation.manage',
+  [VIEWS.RECON_CHANNEL]: 'reconciliation.view',
+  [VIEWS.CHANNEL_RECON_CREATE]: 'reconciliation.manage',
+  [VIEWS.CHANNEL_RECON_EDIT]: 'reconciliation.manage',
+  [VIEWS.CONTRACTS]: 'contracts.view',
+  [VIEWS.INVOICE_MANAGE]: 'invoices.view',
+  [VIEWS.INVOICE_INPUT]: 'invoices.view',
+  [VIEWS.INVOICE_CREATE]: 'invoices.manage',
+  [VIEWS.INVOICE_EDIT]: 'invoices.manage',
+  [VIEWS.QUICKSDK_LIBRARY]: 'data.view',
+  [VIEWS.PRODUCT_SOURCES]: 'data.view',
+  [VIEWS.QUICKSDK_GAMES]: 'data.view',
+  [VIEWS.QUICKSDK_CHANNELS]: 'data.view',
+  [VIEWS.PARTNER_CONTACTS]: 'partners.view'
+})
+
+export function permissionForView(view) {
+  return VIEW_PERMISSIONS[view] || null
+}
+
+export function canOpenView(can, view) {
+  const permission = permissionForView(view)
+  return !permission || Boolean(can?.(permission))
+}
