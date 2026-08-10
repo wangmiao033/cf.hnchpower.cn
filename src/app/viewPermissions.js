e6b8492aa951d78403b7f1cf37a2f73e949bf7f6
wangmiao033 <161289342+wangmiao033@users.noreply.github.com>
2026-08-10 17:37:08 +0800

    feat(finance): protect finance workbench route

--- src/app/viewPermissions.js
+++ src/app/viewPermissions.js
@@ -1,6 +1,7 @@
 import { VIEWS } from '@/app/routes.js'
 
 export const VIEW_PERMISSIONS = Object.freeze({
+  [VIEWS.FINANCE_WORKBENCH]: 'finance_tasks.view',
   [VIEWS.BUSINESS_DASHBOARD]: 'analytics.view',
   [VIEWS.PROFIT_ANALYSIS]: 'analytics.view',
   [VIEWS.ANOMALIES]: 'anomalies.view',
