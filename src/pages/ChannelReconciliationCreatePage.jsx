c5bcb16e359234f2c83214303b151e251f366185
wangmiao033 <161289342+wangmiao033@users.noreply.github.com>
2026-08-18 14:50:31 +0800

    chore: deploy compact channel bill UI

--- src/pages/ChannelReconciliationCreatePage.jsx
+++ src/pages/ChannelReconciliationCreatePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import CoreChannelBillFormPage from './CoreChannelBillFormPage.jsx'
 
+// Display density is handled by the shared core channel bill page.
 function ChannelReconciliationCreatePage() {
   return <CoreChannelBillFormPage mode="create" />
 }
